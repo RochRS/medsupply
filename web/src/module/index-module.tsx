5de1d358045c7735a0e4ebab2c78659e90184d3b
Boogs2002 <hsbajnath@gmail.com>
2026-07-21 18:12:50 -0300

    "Style login page and dashboard"

--- web/src/module/index-module.tsx
+++ web/src/module/index-module.tsx
@@ -1,17 +1,18 @@
 import { Button } from "../components/ui/button";
 import { FormInput } from "../components/global/form-input";
 import { useState } from "react";
+import logo from "../assets/rkz-whitebg.jpeg";
 
 export function HospitalIcon() {
   return (
-    <div>
-      <div>
-        <img />
+    <div className="flex flex-col items-center gap-2 text-center">
+      <div className="bg-indigo-800 rounded-2xl p-4">
+        <img src={logo} alt="RKZ Logo" className="w-12 h-12" />
       </div>
 
       <div>
-        <h1>MedSupply</h1>
-        <p>Voorraadbeheer Systeem voor Medische Supplies</p>
+        <h1 className="text-white text-2xl font-bold">MedSupply</h1>
+        <p className="text-slate-200 text-sm">Voorraadbeheer Systeem voor Medische Supplies</p>
       </div>
     </div>
   );
@@ -30,12 +31,19 @@ export function UserInputFields() {
 
 export function SubmitLoginRequestButton() {
   const sendMessage = () => {
-    console.log("Hellow");
+    console.log("Hello");
   };
 
   return (
-    <Button onClick={sendMessage} disabled={false}>
-      Login
-    </Button>
+    <div className="flex flex-col gap-2 items-center">
+      <Button onClick={sendMessage} disabled={false}>Login</Button>
+      <p className="text-sm text-red-500 text-center">
+        Let op: Alleen medewerkers met een geldig ziekenhuis e-mailadres kunnen inloggen.
+      </p>
+    </div>
+
+    
+
+    
   );
 }
